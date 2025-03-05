1c2de4253f0b56f8fdcf9d64cd187c88ec99a045
Hanna <hanna.svintsitska@gmail.com>
2025-03-05 14:30:20 +0100

    added more tests

--- src/components/store/__tests__/utils.test.ts
+++ src/components/store/__tests__/utils.test.ts
@@ -50,4 +50,49 @@ describe('sortItem', () => {
     ];
     expect(sortItem(initial as Todo[])).toStrictEqual(final);
   });
+  it('should sort all important items on top', () => {
+    const initial: Partial<Todo>[] = [
+      { id: '1', important: true, completed: false },
+      { id: '2', important: true, completed: false },
+      { id: '3', important: true, completed: false },
+      { id: '4', important: true, completed: false },
+    ];
+    const final: Partial<Todo>[] = [
+      { id: '1', important: true, completed: false },
+      { id: '2', important: true, completed: false },
+      { id: '3', important: true, completed: false },
+      { id: '4', important: true, completed: false },
+    ];
+    expect(sortItem(initial as Todo[])).toStrictEqual(final);
+  });
+  it('should sort all completed items on top', () => {
+    const initial: Partial<Todo>[] = [
+      { id: '1', important: false, completed: true },
+      { id: '2', important: false, completed: true },
+      { id: '3', important: false, completed: true },
+      { id: '4', important: false, completed: true },
+    ];
+    const final: Partial<Todo>[] = [
+      { id: '1', important: false, completed: true },
+      { id: '2', important: false, completed: true },
+      { id: '3', important: false, completed: true },
+      { id: '4', important: false, completed: true },
+    ];
+    expect(sortItem(initial as Todo[])).toStrictEqual(final);
+  });
+  it('should sort all equal todos all', () => {
+    const initial: Partial<Todo>[] = [
+      { id: '1', important: false, completed: false },
+      { id: '2', important: false, completed: false },
+      { id: '3', important: false, completed: false },
+      { id: '4', important: false, completed: false },
+    ];
+    const final: Partial<Todo>[] = [
+      { id: '1', important: false, completed: false },
+      { id: '2', important: false, completed: false },
+      { id: '3', important: false, completed: false },
+      { id: '4', important: false, completed: false },
+    ];
+    expect(sortItem(initial as Todo[])).toStrictEqual(final);
+  });
 });
