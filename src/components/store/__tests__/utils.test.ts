d33549eb1ffe873a5535f59ba1926290579ce892
Hanna <hanna.svintsitska@gmail.com>
2025-03-05 14:18:13 +0100

    added unit tests

---
+++ src/components/store/__tests__/utils.test.ts
@@ -0,0 +1,53 @@
+import { Todo } from '../reducer';
+import { sortItem } from '../utils';
+
+describe('sortItem', () => {
+  it('should sort items correctly', () => {
+    const initial: Partial<Todo>[] = [
+      { id: '1', important: false, completed: true },
+      { id: '2', important: false, completed: false },
+      { id: '3', important: true, completed: false },
+      { id: '4', important: true, completed: true },
+    ];
+    const final: Partial<Todo>[] = [
+      { id: '3', important: true, completed: false },
+      { id: '2', important: false, completed: false },
+      { id: '4', important: true, completed: true },
+      { id: '1', important: false, completed: true },
+    ];
+    expect(sortItem(initial as Todo[])).toStrictEqual(final);
+  });
+  it('should return empty array', () => {
+    expect(sortItem([])).toEqual([]);
+  });
+  it('should sort important items on top', () => {
+    const initial: Partial<Todo>[] = [
+      { id: '1', important: false, completed: false },
+      { id: '2', important: false, completed: false },
+      { id: '3', important: true, completed: false },
+      { id: '4', important: true, completed: false },
+    ];
+    const final: Partial<Todo>[] = [
+      { id: '3', important: true, completed: false },
+      { id: '4', important: true, completed: false },
+      { id: '1', important: false, completed: false },
+      { id: '2', important: false, completed: false },
+    ];
+    expect(sortItem(initial as Todo[])).toStrictEqual(final);
+  });
+  it('should sort completed items on bottom', () => {
+    const initial: Partial<Todo>[] = [
+      { id: '1', important: false, completed: false },
+      { id: '2', important: false, completed: true },
+      { id: '3', important: false, completed: false },
+      { id: '4', important: false, completed: true },
+    ];
+    const final: Partial<Todo>[] = [
+      { id: '1', important: false, completed: false },
+      { id: '3', important: false, completed: false },
+      { id: '2', important: false, completed: true },
+      { id: '4', important: false, completed: true },
+    ];
+    expect(sortItem(initial as Todo[])).toStrictEqual(final);
+  });
+});
--- src/components/store/utils.ts
+++ src/components/store/utils.ts
@@ -3,6 +3,6 @@ import { Todo } from './reducer';
 export const sortItem = (todo: Todo[]): Todo[] => {
   return todo.sort(
     (el1, el2) =>
-      +el2.important - +el1.important || +el1.completed - +el2.completed
+      +el1.completed - +el2.completed || +el2.important - +el1.important
   );
 };
